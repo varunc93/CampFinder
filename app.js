747553b8a002b8f50fc5060ad8d7a4ed603cec24
varunc93 <reachvc93@gmail.com>
2018-05-13 07:15:36 +0000

    Mongolab changes and Flash message changes

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp");       
+//mongoose.connect("mongodb://localhost/camp_finder");
+mongoose.connect("mongodb://campfinder:campfinder@ds121960.mlab.com:21960/campfinder");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@ router.post("/register", function(req, res) {
             return res.render("register", {page: 'register'});
         }
         passport.authenticate("local")(req, res, function(){
-            req.flash("success", "Welcome to CampFinder!" +req.body.username);
+            req.flash("success", "Welcome to CampFinder, " + req.body.username + "!");
             res.redirect("/campgrounds");    
         });
     }); 
