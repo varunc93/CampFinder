7690cbf67ff223c1af8a23e0e179ce2649026f11
varunc93 <reachvc93@gmail.com>
2018-05-13 07:41:50 +0000

    Backup url

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-mongoose.connect(process.env.DATABASEURL);
+var url = process.env.DATABASEURL || "mongodb://localhost/camp_finder";
+mongoose.connect(url);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
